284a65d4c66b48a57445d094f9ef8e61d1550a7a
Alec <a.palmer@elsevier.com>
2020-11-05 15:34:11 -0500

    test commit 2 11052020

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from profiles_api import views
 
 
-url_patterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),c
 ]
